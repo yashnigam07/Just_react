bc821d7341ff329ea0f55b950a076027de831685
yashnigam07 <chatgpt7070@gmail.com>
2024-07-23 23:37:51 +0530

    REact frits commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React Projects & Shenanigans!</h1>
+      <p>This repo is packed with React projects and components that are as entertaining as they are educational.</p>
+      <p>Whether you're here to learn, reference, or just marvel at the madness, there's something for everyone.</p>
+      <p>Let's build some epic UIs and have a blast while we're at it!</p>
+      <p>Happy coding! 🚀</p>
+    </div>
+  );
+}
+
+export default App;
